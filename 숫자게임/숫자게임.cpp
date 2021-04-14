6bcffd74b52736f6dd18c44d0e29270b4858c2d8
DahamChoi <rorigu32@gmail.com>
2021-04-14 20:56:21 +0900

    숫자게임

---
+++ 숫자게임/숫자게임.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> A, vector<int> B) {
+    int answer = A.size(), i, j;
+
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+
+    for (i = 0, j = 0; i < A.size() && j < B.size();) {
+        if (A[i] > B[j]) {
+            ++j;
+            --answer;
+        }
+        else if (A[i] == B[j]) {
+            --answer;
+            ++j;
+        } else {
+            ++i;
+            ++j;
+        }
+    }
+
+    return answer;
+}
+
+int main() {
+    vector<int> A = { 5,1,3,7 };
+    vector<int> B = { 2,2,6,8 };
+    cout << solution(A, B);
+}
