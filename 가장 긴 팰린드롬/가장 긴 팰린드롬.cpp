c09ea3015b7a1ec48543d78113afd1769888f7be
DahamChoi <rorigu32@gmail.com>
2021-04-22 18:51:05 +0900

    가장 긴 팰린드롬

---
+++ 가장 긴 팰린드롬/가장 긴 팰린드롬.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int solution(string s)
+{
+    int answer = 0;
+
+    for (int i = 0; i < s.length() - answer; i++) {
+        for (int j = s.length() - 1; j >= i; j--) {
+            int left = i, right = j;
+            while (left + 1 < right - 1 && s[left] == s[right]) {
+                ++left;
+                --right;
+            }
+
+            answer = max(answer, (j - i) + 1);
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    std::cout << solution("abcdcba");
+}
