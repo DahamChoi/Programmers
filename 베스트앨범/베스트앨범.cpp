28c5d7f96922bb7b3b8c62c7128217721cff471f
DahamChoi <rorigu32@gmail.com>
2021-04-22 18:50:10 +0900

    베스트 앨범

---
+++ 베스트앨범/베스트앨범.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+vector<int> solution(vector<string> genres, vector<int> plays) {
+    vector<int> answer;
+
+    unordered_map<string, pair<int, vector<pair<int, int>>>> play_map;
+    for (int i = 0; i < genres.size(); i++) {
+        play_map[genres[i]].first += plays[i];
+        play_map[genres[i]].second.push_back({ plays[i], i });
+    }
+
+    for (auto it = play_map.begin(); it != play_map.end(); it++) {
+        sort((*it).second.second.begin(), (*it).second.second.end(), [](const pair<int, int>& p1, const pair<int, int>& p2) {
+            return p1.first > p2.first;
+            });
+    }
+
+    vector<pair<string, pair<int, vector<pair<int, int>>>>> play_list(play_map.begin(), play_map.end());
+    sort(play_list.begin(), play_list.end(), [](const pair<string, pair<int, vector<pair<int, int>>>>& p1,
+        const pair<string, pair<int, vector<pair<int, int>>>>& p2) {
+            return p1.second.first > p2.second.first;
+        });
+
+    for (int i = 0; i < play_list.size(); i++) {
+        for (int j = 0; j < play_list[i].second.second.size() && j < 2; j++) {
+            answer.push_back(play_list[i].second.second[j].second);
+        }
+    }
+
+    return answer;
+}
+
+int main()
+{
+    vector<int> answer = solution({ "classic","pop","classic","classic","pop" }, { 500,600,150,800,2500 });
+    for (int i = 0; i < answer.size(); i++) {
+        cout << answer[i] << " ";
+    }
+}
---
+++ 최고의집합/최고의집합.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> solution(int n, int s) {
+    vector<int> answer;
+    vector<int> numbers(n);
+
+    int max_number = 0;
+
+    for (int i = 0; i < n; i++) {
+
+    }
+
+    return answer;
+}
+
+int main() {
+    std::cout << solution(2, 8)[0];
+}
