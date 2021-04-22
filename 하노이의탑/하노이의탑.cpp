228a10e6dd49a5e95a0cd869c0f803ed772d8b3f
DahamChoi <rorigu32@gmail.com>
2021-04-22 13:05:02 +0900

    하노이의탑

--- 하노이의탑/하노이의탑.cpp
+++ 하노이의탑/하노이의탑.cpp
@@ -1,20 +1,96 @@
-﻿// 하노이의탑.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
+#include <vector>
+using namespace std;
 
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+int next_tower(int num, int a) {
+    return (num + a) >= 3 ? (num + a - 3) : num + a;
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+void next_one_step(vector<vector<int>>& answer, int index) {
+    answer.push_back({ index + 1, next_tower(index, 2) + 1 });
+    answer.push_back({ index + 1, next_tower(index, 1) + 1 });
+    answer.push_back({ next_tower(index, 2) + 1, next_tower(index, 1) + 1 });
+}
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+void next_two_step(vector<vector<int>>& answer, int index) {
+    answer.push_back({ index + 1, next_tower(index, 1) + 1 });
+    answer.push_back({ index + 1, next_tower(index, 2) + 1 });
+    answer.push_back({ next_tower(index, 1) + 1, next_tower(index, 2) + 1 });
+}
+
+void tower_step(vector<vector<int>>& answer, int from, int to, int n) {
+    int step = (to - from < 0) ? (to - from == -2) ? 1 : 2 : to - from;
+
+    if (step == 2 && n == 2) {
+        next_two_step(answer, from);
+    }
+    else if (step == 1 && n == 2) {
+        next_one_step(answer, from);
+    }
+    else {
+        tower_step(answer, from, next_tower(from, step == 2 ? 1 : 2), n - 1);
+        answer.push_back({ from + 1, to + 1 });
+        tower_step(answer, next_tower(from, step == 2 ? 1 : 2), to, n - 1);
+    }
+}
+
+vector<vector<int>> solution(int n) {
+    vector<vector<int>> answer;
+    tower_step(answer, 0, 2, n);
+    return answer;
+}
+
+int main() {
+    vector<vector<int>> answer = solution(4);
+    for (int i = 0; i < answer.size(); i++) {
+        cout << answer[i][0] << "," << answer[i][1] << endl;
+    }
+}
+
+/*
+
+(one step)
+1,3
+1,2
+3,2
+
+1,3
+
+(one step)
+2,1
+2,3
+1,3
+------------------------------------
+
+(two step)
+1,2
+1,3
+2,3
+
++1
+1,2
+
+(two step)
+3,1
+3,2
+1,2
+
++2
+1,3
+
+(two step)
+2,3
+2,1
+3,1
+
++1
+2,3
+
+(two step)
+1,2
+1,3
+2,3
+
+
+
+*/
