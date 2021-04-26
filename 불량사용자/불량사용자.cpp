a2d9c3873b751dbd89fa5b25f6a0d344dee7cba8
DahamChoi <rorigu32@gmail.com>
2021-04-26 19:58:00 +0900

    불량 사용자

---
+++ 불량사용자/불량사용자.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<vector<string>> select_id_list;
+
+void search_banned(vector<vector<string>>& banned_id_list, vector<string>& select_id, int index) {
+    for (int i = 0; i < banned_id_list[index].size(); i++) {
+        bool checker = true;
+        for (int j = 0; j < select_id.size(); j++) {
+            if (banned_id_list[index][i] == select_id[j]) {
+                checker = false;
+                break;
+            }
+        }
+
+        if (checker) {
+            select_id.push_back(banned_id_list[index][i]);
+            if (index == banned_id_list.size() - 1) {
+                select_id_list.push_back(select_id);
+            }
+            else {
+                search_banned(banned_id_list, select_id, index + 1);
+            }
+            select_id.erase(select_id.end() - 1);
+        }
+    }
+}
+
+int solution(vector<string> user_id, vector<string> banned_id) {
+    vector<vector<string>> banned_id_list(banned_id.size());
+
+    for (int i = 0; i < banned_id.size(); i++) {
+        for (int j = 0; j < user_id.size(); j++) {
+            if (banned_id[i].length() == user_id[j].length()) {
+                bool checker = true;
+                for (int k = 0; k < banned_id[i].length() && k < user_id[j].length(); k++) {
+                    if (banned_id[i][k] != '*' && banned_id[i][k] != user_id[j][k]) {
+                        checker = false;
+                        break;
+                    }
+                }
+
+                if (checker) {
+                    banned_id_list[i].push_back(user_id[j]);
+                }
+            }
+        }
+    }
+
+    vector<string> select_id;
+    search_banned(banned_id_list, select_id, 0);
+
+    for (int i = 0; i < select_id_list.size(); i++) {
+        sort(select_id_list[i].begin(), select_id_list[i].end());
+    }
+
+    sort(select_id_list.begin(), select_id_list.end());
+    select_id_list.erase(unique(select_id_list.begin(), select_id_list.end()), select_id_list.end());
+
+    return select_id_list.size();
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
---
+++ 스타수열/스타수열.cpp
@@ -0,0 +1,45 @@
+﻿#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> a) {
+    for (int cnt = (a.size() - (a.size() % 2)) - 1; cnt >= 1; cnt -= 2) {
+        vector<int> cnd(a.size(), 1);
+        for (int i = 0; i <= cnt; i++) {
+            cnd[i] = 0;
+        }
+
+        do {
+            unordered_map<int, int> item_list;
+            for (int i = 0; i < cnd.size(); i++) {
+                if (cnd[i] == 0) {
+                    ++item_list[a[i]];
+                    cout << a[i] << " ";
+                }
+            }
+            cout << endl;
+
+            bool checker = false;
+            for (auto it = item_list.begin(); it != item_list.end(); it++) {
+                if (!(it->second == ((cnt + 1) / 2) || it->second == 1)) {
+                    checker = true;
+                    break;
+                }
+            }
+
+            if (!checker) {
+                return cnt + 1;
+            }
+        } while (next_permutation(cnd.begin(), cnd.end()));
+    }
+
+    return 0;
+}
+
+int main()
+{
+    std::cout << solution({ 0, 3, 3, 0, 7, 2, 0, 2, 2, 0 });
+}
