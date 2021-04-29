6091484b8ffb77f89dc9d3fc9c190d61ace80503
DahamChoi <rorigu32@gmail.com>
2021-04-29 21:51:35 +0900

    카드짝 맞추기

---
+++ 블록이동하기/블록이동하기.cpp
@@ -0,0 +1,109 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+struct robot {
+    int x1, x2, y1, y2;
+};
+
+int answer = INT_MAX;
+
+bool is_rotate(vector<vector<int>>& board, robot& bot, int angle) {
+    if (angle == 0) {
+        int new_x = bot.x2 - 1;
+        int new_y = bot.y2 + 1;
+        if (new_x >= 0 && new_x < board.size() && new_y >= 0 && new_y < board.size() &&
+            board[new_y][new_x] == 0) {
+            return true;
+        }
+    }
+    else if (angle == 1) {
+        int new_x = bot.x2 - 2;
+        int new_y = bot.y2;
+        if (new_x >= 0 && new_x < board.size() && new_y >= 0 && new_y < board.size() &&
+            board[new_y][new_x] == 0) {
+            return true;
+        }
+    }
+    else if (angle == 2) {
+        int new_x = bot.x2 - 1;
+        int new_y = bot.y2 - 1;
+        if (new_x >= 0 && new_x < board.size() && new_y >= 0 && new_y < board.size() &&
+            board[new_y][new_x] == 0) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+robot rotate(robot& bot, int angle) {
+    robot new_bot = bot;
+    if (angle == 0) {
+        --new_bot.x2;
+        ++new_bot.y2;
+    }
+    else if (angle == 1) {
+        new_bot.x2 -= 2;
+    }
+    else if (angle == 2) {
+        --new_bot.x2;
+        --new_bot.y2;
+    }
+
+    return new_bot;
+}
+
+void search(vector<vector<int>>& board, vector<vector<bool>> visit, robot bot, int depth) {
+    if (bot.x1 < 0 || bot.x2 < 0 || bot.y1 < 0 || bot.y2 < 0 ||
+        bot.x1 >= board.size() || bot.x2 >= board.size() || bot.y1 >= board.size() || bot.y2 >= board.size()) {
+        return;
+    }
+
+    if (visit[bot.y1][bot.x1] && visit[bot.y2][bot.x2]) {
+        return;
+    }
+
+    visit[bot.y1][bot.x1] = true;
+    visit[bot.y2][bot.x2] = true;
+
+    if ((bot.x1 == board.size() - 1 && bot.y1 == board.size() - 1) ||
+        (bot.x2 == board.size() - 1 && bot.y2 == board.size() - 1)) {
+        answer = min(answer, depth);
+        return;
+    }
+
+    if (board[bot.x1][bot.y1] == 1 || board[bot.x2][bot.y2] == 1) {
+        return;
+    }
+
+    for (int i = 0; i < 3; i++) {
+        if (is_rotate(board, bot, i)) {
+            robot new_bot = rotate(bot, i);
+            search(board, visit, { new_bot.x1 + 1, new_bot.x2 + 1, new_bot.y1, new_bot.y2 }, depth + 1);
+            search(board, visit, { new_bot.x1 - 1, new_bot.x2 - 1, new_bot.y1, new_bot.y2 }, depth + 1);
+            search(board, visit, { new_bot.x1, new_bot.x2, new_bot.y1 + 1, new_bot.y2 + 1 }, depth + 1);
+            search(board, visit, { new_bot.x1, new_bot.x2, new_bot.y1 - 1, new_bot.y2 - 1 }, depth + 1);
+        }
+    }
+
+    search(board, visit, { bot.x1 + 1, bot.x2 + 1, bot.y1, bot.y2 }, depth + 1);
+    search(board, visit, { bot.x1 - 1, bot.x2 - 1, bot.y1, bot.y2 }, depth + 1);
+    search(board, visit, { bot.x1, bot.x2, bot.y1 + 1, bot.y2 + 1 }, depth + 1);
+    search(board, visit, { bot.x1, bot.x2, bot.y1 - 1, bot.y2 - 1 }, depth + 1);
+}
+
+int solution(vector<vector<int>> board) {
+    vector<vector<bool>> visit(board.size(), vector<bool>(board.size()));
+    search(board, visit, { 0,1,0,0 }, 0);
+
+    return answer;
+}
+
+int main()
+{
+    std::cout << solution({ {0, 0, 0, 1, 1},{0, 0, 0, 1, 0},{0, 1, 0, 1, 1},{1, 1, 0, 0, 1},{0, 0, 0, 0, 0} });
+}
--- 카드짝 맞추기/소스.cpp
+++ 카드짝 맞추기/소스.cpp
@@ -1,4 +1,5 @@
-﻿#include <iostream>
+/*
+
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -6,16 +7,12 @@
 
 using namespace std;
 
-struct path_finder {
-    int x, y, depth;
-};
-
 pair<int, int> ctrl_move(vector<vector<int>>& board, int cursor_x, int cursor_y, int direction_x, int direction_y) {
     cursor_x += direction_x;    cursor_y += direction_y;
-    while (cursor_x + direction_x < board.size() && 
-        cursor_x + direction_x >= 0 && 
-        cursor_y + direction_y < board.size() && 
-        cursor_y + direction_y >= 0 && 
+    while (cursor_x + direction_x < board.size() &&
+        cursor_x + direction_x >= 0 &&
+        cursor_y + direction_y < board.size() &&
+        cursor_y + direction_y >= 0 &&
         board[cursor_y][cursor_x] == 0) {
         cursor_x += direction_x;
         cursor_y += direction_y;
@@ -24,47 +21,32 @@ pair<int, int> ctrl_move(vector<vector<int>>& board, int cursor_x, int cursor_y,
     return { cursor_x, cursor_y };
 }
 
-void path_find(vector<vector<int>>& board, vector<vector<bool>>& visit, int x, int y, int goal_x, int goal_y, int depth, int& min_depth) {
-    if (x < 0 || x >= board.size() || y < 0 || y >= board.size()) {
-        return;
-    }
-
-    if (depth >= min_depth) {
-        return;
-    }
-
-    if (x == goal_x && y == goal_y) {
-        min_depth = min(depth, min_depth);
-        return;
-    }
-
-    if (visit[y][x]) {
-        return;
-    }
-
-    visit[y][x] = true;
-    path_find(board, visit, x + 1, y, goal_x, goal_y, depth + 1, min_depth);
-    path_find(board, visit, x - 1, y, goal_x, goal_y, depth + 1, min_depth);
-    path_find(board, visit, x, y + 1, goal_x, goal_y, depth + 1, min_depth);
-    path_find(board, visit, x, y - 1, goal_x, goal_y, depth + 1, min_depth);
-    pair<int, int> p1 = ctrl_move(board, x, y, 1, 0);
-    pair<int, int> p2 = ctrl_move(board, x, y, -1, 0);
-    pair<int, int> p3 = ctrl_move(board, x, y, 0, 1);
-    pair<int, int> p4 = ctrl_move(board, x, y, 0, -1);
-    path_find(board, visit, p1.first, p1.second, goal_x, goal_y, depth + 1, min_depth);
-    path_find(board, visit, p2.first, p2.second, goal_x, goal_y, depth + 1, min_depth);
-    path_find(board, visit, p3.first, p3.second, goal_x, goal_y, depth + 1, min_depth);
-    path_find(board, visit, p4.first, p4.second, goal_x, goal_y, depth + 1, min_depth);
-    visit[y][x] = false;
-}
-
 int get_move_length(vector<vector<int>>& board, int cursor_x, int cursor_y, int goal_x, int goal_y) {
     int default_length_x = abs(cursor_x - goal_x);
     int default_length_y = abs(cursor_y - goal_y);
     int length = default_length_x + default_length_y;
 
-    vector<vector<bool>> visit(board.size(), vector<bool>(board.size()));
-    path_find(board, visit, cursor_x, cursor_y, goal_x, goal_y, 0, length);
+    pair<int,int> p1 = ctrl_move(board, cursor_x, cursor_y, cursor_x - goal_x < 0 ? 1 : -1, 0);
+    pair<int,int> p2 = ctrl_move(board, p1.first, p1.second, 0, p1.second - goal_y < 0 ? 1 : -1);
+    if (p2.first == goal_x && p2.second == goal_y) {
+        length = min(length, 2);
+    }
+
+    pair<int, int> p3 = ctrl_move(board, goal_x, cursor_y, 0, cursor_y - goal_y < 0 ? 1 : -1);
+    if (p3.first == goal_x && p3.second == goal_y) {
+        length = min(length, default_length_x + 1);
+    }
+
+    pair<int, int> p4 = ctrl_move(board, cursor_x, goal_y, cursor_x - goal_x < 0 ? 1 : -1, 0);
+    if (p4.first == goal_x && p4.second == goal_y) {
+        length = min(length, default_length_y + 1);
+    }
+
+    pair<int, int> p5 = ctrl_move(board, cursor_x, cursor_y, 0, cursor_y - goal_y < 0 ? 1 : -1);
+    pair<int, int> p6 = ctrl_move(board, p5.first, p5.second, p1.first - goal_x < 0 ? 1 : -1, 0);
+    if (p6.first == goal_x && p6.second == goal_y) {
+        length = min(length, 2);
+    }
 
     return length;
 }
@@ -80,8 +62,8 @@ int remove_pair_card(vector<vector<int>>& board, int card_number, int cursor_x, 
         }
     }
 
-    int move_length_1 = 
-        get_move_length(board, cursor_x, cursor_y, target_card_position.front().first, target_card_position.front().second) + 
+    int move_length_1 =
+        get_move_length(board, cursor_x, cursor_y, target_card_position.front().first, target_card_position.front().second) +
         get_move_length(board, target_card_position.front().first, target_card_position.front().second, target_card_position.back().first, target_card_position.back().second);
     int move_length_2 =
         get_move_length(board, cursor_x, cursor_y, target_card_position.back().first, target_card_position.back().second) +
@@ -102,7 +84,7 @@ int remove_pair_card(vector<vector<int>>& board, int card_number, int cursor_x, 
 
 int solution(vector<vector<int>> board, int r, int c) {
     int answer = INT_MAX;
-    
+
     vector<int> card_numbers;
     for (int i = 0; i < board.size(); i++) {
         for (int j = 0; j < board.size(); j++) {
@@ -128,6 +110,4 @@ int solution(vector<vector<int>> board, int r, int c) {
     return answer;
 }
 
-int main() {
-    std::cout << solution({ {1, 0, 0, 3},{2, 0, 0, 0},{0, 0, 0, 2},{3, 0, 1, 0 }}, 1, 0);
-}
+*/
