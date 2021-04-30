8f7c4b2031009e2208d24350c1e8d7e95d9ca014
DahamChoi <rorigu32@gmail.com>
2021-04-30 18:53:19 +0900

    N진수게임

---
+++ N진수게임/N진수게임.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+string solution(int n, int t, int m, int p) {
+    string answer = "";
+    map<int, char> number_map = { {0,'0'}, {1,'1'}, {2,'2'}, {3,'3'}, {4,'4'},
+                     {5,'5'}, {6,'6'}, {7,'7'}, {8,'8'}, {9,'9'},
+                     {10,'A'}, {11,'B'}, {12,'C'}, {13,'D'}, {14,'E'}, {15, 'F'} };
+
+    string counter;
+    for (int i = 0; i <= m * t; i++) {
+        int radix = n, decimal = i;
+        string number_str;
+        while (decimal >= radix) {
+            number_str += number_map[decimal % radix];
+            decimal /= radix;
+        }
+        number_str += number_map[decimal % radix];
+        reverse(number_str.begin(), number_str.end());
+        counter += number_str;
+    }
+
+    for (int i = 0; i < t; i++) {
+        int cnt = (m * i) + p - 1;
+        answer += counter[cnt];
+    }
+
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
---
+++ 튜플/튜플.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(string s) {
+    vector<int> answer;
+
+    vector<vector<int>> list;
+
+    for (int i = 1; i < s.length() - 1;) {
+        if (s[i] == '{') {
+            vector<int> temp_list;
+            int j = i + 1;
+            while (j < s.length() && s[j] != '}') {
+                int k = j;
+                while ((k = s.find(',', k)) != string::npos) {
+                    temp_list.push_back(atoi(s.substr(j, k - j).c_str()));
+                    j = k + 1;
+                }
+            }
+            list.push_back(temp_list);
+            i = j + 1;
+        }
+    }
+
+    for (int i = 0; i < list.size(); i++) {
+        for (int j = 0; j < list[i].size(); j++) {
+            cout << list[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return answer;
+}
+
+int main()
+{
+    solution("{{2},{2,1},{2,1,3},{2,1,3,4}}");
+}
