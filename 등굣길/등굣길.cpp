2d9d169ac35fed6327ee83fe51710781e93d1e2e
DahamChoi <rorigu32@gmail.com>
2021-05-06 20:48:53 +0900

    등굣길

---
+++ 등굣길/등굣길.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int solution(int m, int n, vector<vector<int>> puddles) {
+    int answer = 0;
+
+    vector<vector<int>> dp(n, vector<int>(m)), map(n, vector<int>(m));
+
+    for (int i = 0; i < puddles.size(); i++) {
+        map[puddles[i][1] - 1][puddles[i][0] - 1] = 1;
+    }
+
+    if (map[1][0] == 0) {
+        dp[1][0] = 1;
+    }
+    if (map[0][1] == 0) {
+        dp[0][1] = 1;
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (map[i][j] == 1) {
+                dp[i][j] = 0;
+            }
+            else {
+                if (i - 1 >= 0) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j]) % 1000000007;
+                }
+                if (j - 1 >= 0) {
+                    dp[i][j] = (dp[i][j] + dp[i][j - 1]) % 1000000007;
+                }
+            }
+        }
+    }
+
+    return dp[n - 1][m - 1];
+}
+
+int main()
+{
+    std::cout << solution(4, 3, { {2,2} });
+}
---
+++ 이중우선순위큐/이중우선순위큐.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> operations) {
+    vector<int> answer = { 0, 0 }, double_prioperty_queue;
+
+    for (int i = 0; i < operations.size(); i++) {
+        if (operations[i].front() == 'I') {
+            int number = atoi(operations[i].substr(2, operations[i].size() - 2).c_str());
+            double_prioperty_queue.push_back(number);
+        }
+        else if (operations[i].front() == 'D' && !double_prioperty_queue.empty()) {
+            sort(double_prioperty_queue.begin(), double_prioperty_queue.end());
+
+            if (operations[i][2] == '1') {
+                double_prioperty_queue.pop_back();
+            }
+            else if (operations[i][2] == '-') {
+                double_prioperty_queue.erase(double_prioperty_queue.begin());
+            }
+        }
+    }
+
+    sort(double_prioperty_queue.begin(), double_prioperty_queue.end());
+    if (!double_prioperty_queue.empty()) {
+        answer = { double_prioperty_queue.front(), double_prioperty_queue.back() };
+    }
+
+    return answer;
+}
+
+int main() {
+    vector<int> answer = solution({ "I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1" });
+    cout << answer[0] << "," << answer[1] << endl;
+}
