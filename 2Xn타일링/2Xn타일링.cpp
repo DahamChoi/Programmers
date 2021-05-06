7e4e28206a6e74a6547511f09d7fa0091e3a3e1c
DahamChoi <rorigu32@gmail.com>
2021-05-06 22:50:32 +0900

    정수 삼각형

---
+++ 2Xn타일링/2Xn타일링.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+unsigned combination(unsigned n, unsigned k) {
+    if (k > n) return 0;
+    if (k * 2 > n) k = n - k;
+    if (k == 0) return 1;
+
+    int result = n;
+    for (int i = 2; i <= k; ++i) {
+        result *= (n - i + 1);
+        result /= i;
+    }
+    return result;
+}
+
+int solution(int n) {
+    int answer = 0;
+
+    for (int i = n % 2; i <= n; i += 2) {
+        answer += combination(i + ((n - i) / 2), i);
+    }
+
+    return answer;
+}
+
+int main()
+{
+    for (int i = 1; i < 10; i++) {
+        std::cout << solution(i) << endl;
+    }
+}
---
+++ 정수삼각형/정수삼각형.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> triangle) {
+    int answer = 0, h = triangle.size();
+
+    vector<vector<int>> dp(h, vector<int>(h));
+    dp[0][0] = triangle[0][0];
+
+    for (int i = 1; i < h; i++) {
+        for (int j = 0; j < triangle[i].size(); j++) {
+            if (j - 1 >= 0) {
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - 1]) + triangle[i][j];
+            }
+            else {
+                dp[i][j] = dp[i - 1][j] + triangle[i][j];
+            }
+        }
+    }
+
+    for (int i = 0; i < h; i++) {
+        answer = max(answer, dp.back()[i]);
+    }
+
+    return answer;
+}
+
+int main()
+{
+    std::cout << solution({ {7},{3,8},{8,1,0},{2,7,4,4},{4,5,2,6,5} });
+}
